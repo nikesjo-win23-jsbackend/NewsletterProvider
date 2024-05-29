3ddd78edf2872d4ab9e9121420239772bd53c22a
Nike <nikesjolin@outlook.com>
2024-05-30 00:20:38 +0200

    Add project files.

---
+++ Data/Contexts/DataContextFactory.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore.Design;
+using Microsoft.EntityFrameworkCore;
+
+namespace Data.Contexts;
+
+public class DataContextFactory : IDesignTimeDbContextFactory<DataContext>
+{
+    public DataContext CreateDbContext(string[] args)
+    {
+        var optionsBuilder = new DbContextOptionsBuilder<DataContext>();
+        optionsBuilder.UseSqlServer("Server=tcp:sql-win23jsbackend-nikesjo.database.windows.net,1433;Initial Catalog=jsbackend_database;Persist Security Info=False;User ID=SqlAdmin;Password=Server!Losenord123;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;");
+
+        return new DataContext(optionsBuilder.Options);
+    }
+}
